e3d495ea29a3f8f2f4dbe33de4c0d4e7ebff66a1
Andrey Arkhipov <arhipov11@yandex.ru>
2022-04-02 20:46:51 +0300

    Create header

---
+++ scripts/components/Header.js
@@ -0,0 +1,18 @@
+export const btnCloseMenu = document.querySelector(".header__close-icon");
+export const btnBurgerMenu = document.querySelector(".header__burger-menu");
+const headerMenu = document.querySelector(".header__menu");
+const logo = document.querySelector(".header__logo");
+
+export function handleClickCloseMenu() {
+  headerMenu.classList.remove("header__menu_active");
+  btnCloseMenu.classList.remove("header__close-icon_active");
+  logo.classList.remove("header__logo_menu_open");
+  btnBurgerMenu.classList.remove("header__burger-menu_disabled");
+}
+
+export function handleClickOpenMenu() {
+  headerMenu.classList.add("header__menu_active");
+  btnCloseMenu.classList.add("header__close-icon_active");
+  logo.classList.add("header__logo_menu_open");
+  btnBurgerMenu.classList.add("header__burger-menu_disabled");
+}
