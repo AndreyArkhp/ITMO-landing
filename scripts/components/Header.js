2f2c9d192a52396b6b7fa99145447c6cc23a4256
Andrey Arkhipov <arhipov11@yandex.ru>
2022-04-05 09:45:15 +0300

    Fix: variable names changed

--- scripts/components/Header.js
+++ scripts/components/Header.js
@@ -1,20 +1,21 @@
 export const btnCloseMenu = document.querySelector(".header__close-icon");
 export const btnBurgerMenu = document.querySelector(".header__burger-menu");
 export const linkContainer = document.querySelector(".header__menu-item_type_container");
-const headerMenu = document.querySelector(".header__menu");
+const menu = document.querySelector(".header__menu");
 const logo = document.querySelector(".header__logo");
-const headerNestedList = document.querySelector(".header__nested-list");
+const nestedList = document.querySelector(".header__nested-list");
+const menuItems = document.querySelectorAll(".header__menu-item");
 let isOpened = false;
 
 export function handleClickCloseMenu() {
-  headerMenu.classList.remove("header__menu_active");
+  menu.classList.remove("header__menu_active");
   btnCloseMenu.classList.remove("header__close-icon_active");
   logo.classList.remove("header__logo_menu_open");
   btnBurgerMenu.classList.remove("header__burger-menu_disabled");
 }
 
 export function handleClickOpenMenu() {
-  headerMenu.classList.add("header__menu_active");
+  menu.classList.add("header__menu_active");
   btnCloseMenu.classList.add("header__close-icon_active");
   logo.classList.add("header__logo_menu_open");
   btnBurgerMenu.classList.add("header__burger-menu_disabled");
@@ -22,10 +23,16 @@ export function handleClickOpenMenu() {
 
 export function openLinkContainer() {
   if (!isOpened) {
-    headerNestedList.classList.add("header__nested-list_opened");
+    nestedList.classList.add("header__nested-list_opened");
+    menuItems.forEach((item) => {
+      item.classList.add("header__menu-item_type_dark");
+    });
     isOpened = true;
   } else {
-    headerNestedList.classList.remove("header__nested-list_opened");
+    nestedList.classList.remove("header__nested-list_opened");
+    menuItems.forEach((item) => {
+      item.classList.remove("header__menu-item_type_dark");
+    });
     isOpened = false;
   }
 }
