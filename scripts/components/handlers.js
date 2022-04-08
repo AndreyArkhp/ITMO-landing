27fbf83921cbf6126896209c9cc7ec09a258afdb
Andrey Arkhipov <arhipov11@yandex.ru>
2022-04-08 15:50:31 +0300

    Amended handler click

--- scripts/components/handlers.js
+++ scripts/components/handlers.js
@@ -7,19 +7,16 @@ import {
   openLinkContainer,
 } from "./Header.js";
 
-import { handleClickCard, cards } from "./researchlab.js";
+import { handleClickCard, containerCards } from "./researchlab.js";
 import { form, handlerSubmit } from "./form.js";
 
 import { handleBtnClick, studyBtns } from "./study.js";
-import { form, handlerSubmit } from "./form.js";
 
 function setEventListener() {
   btnCloseMenu.addEventListener("click", handleClickCloseMenu);
   btnBurgerMenu.addEventListener("click", handleClickOpenMenu);
   linkContainer.addEventListener("click", openLinkContainer);
-  cards.forEach((card) => {
-    card.addEventListener("click", handleClickCard);
-  });
+  containerCards.addEventListener("click", handleClickCard);
   studyBtns.forEach((btn) => {
     btn.addEventListener("click", handleBtnClick);
   });
--- scripts/components/researchlab.js
+++ scripts/components/researchlab.js
@@ -1,11 +1,14 @@
-export const cards = document.querySelectorAll(".card-lab");
+export const containerCards = document.querySelector(".researchlab__container");
 let cardActive;
 
 export function handleClickCard(event) {
-  const card = event.currentTarget;
-  if (!card.classList.contains("card-lab_active")) {
+  const card = event.target.closest(".card-lab");
+  if (card && !event.target.classList.contains("card-lab_active")) {
     cardActive && cardActive.classList.remove("card-lab_active");
     card.classList.add("card-lab_active");
     cardActive = card;
+  }
+  if (!card) {
+    cardActive.classList.remove("card-lab_active");
   }
 }
