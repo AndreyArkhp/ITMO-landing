4d0000a38831224aefb171eb1a7e32073f12ec97
Andrey Arkhipov <arhipov11@yandex.ru>
2022-04-08 21:00:44 +0300

    Added desktop version

--- scripts/components/researchlab.js
+++ scripts/components/researchlab.js
@@ -1,14 +1,17 @@
-export const containerCards = document.querySelector(".researchlab__container");
+export const containerCards = document.querySelector(".researchlab");
 let cardActive;
 
 export function handleClickCard(event) {
   const card = event.target.closest(".card-lab");
-  if (card && !event.target.classList.contains("card-lab_active")) {
+  if (card && !card.classList.contains("card-lab_active")) {
     cardActive && cardActive.classList.remove("card-lab_active");
     card.classList.add("card-lab_active");
     cardActive = card;
+  } else if (card && card.classList.contains("card-lab_active")) {
+    cardActive.classList.remove("card-lab_active");
+    cardActive = null;
   }
   if (!card) {
-    cardActive.classList.remove("card-lab_active");
+    cardActive && cardActive.classList.remove("card-lab_active");
   }
 }
